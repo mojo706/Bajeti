2347be68f18604c492007fc355cc1833e41d6824
chuchu <eugene.omar@andela.com>
2017-10-10 10:32:36 +0300

    convert BudgetItem into a class component

--- src/components/BudgetItemForm.js
+++ src/components/BudgetItemForm.js
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const BudgetItemForm = (props) => (
+class BudgetItemForm extends Component {
 
-  <form action="">
-    <h4>Enter a Budget Item</h4>
-    <input type="text" name="item" placeholder="Enter Budget item" />
-    <input type="text" name="amount" placeholder="Enter Budget amount" />
-    <button>Save Item</button>
-  </form>
+  titleInput;
+  amountInput;
 
-);
+  constructor(props) {
+    super(props);
+    this.state = {
+      item:
+      {
+
+        title: '',
+        amount: 0
+      }
+    };
+
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.addBudgetItems(this.state.item)
+  };
+
+  render() {
+    return (
+      <form action="" >
+        <h4>Enter a Budget Item</h4>
+        <input
+          type="text"
+          name="title"
+          ref={(input) => { this.titleInput = input }}
+          placeholder="Enter Budget item"
+        />
+        <input
+          type="number"
+          name="amount"
+          ref={(input) => { this.amountInput = input }}
+          placeholder="Enter Budget amount"
+
+        />
+        <button >Save Item</button>
+      </form>
+
+    )
+  }
+
+};
 
 export default BudgetItemForm;
