6a4876c794ae6e95364d0ca52ad51757dd01bdd3
chuchu <eugene.omar@andela.com>
2017-10-10 10:34:18 +0300

    refactor the ItemForm component

--- src/components/BudgetView.js
+++ src/components/BudgetView.js
@@ -2,37 +2,39 @@ import React from 'react';
 
 import BudgetItemForm from './BudgetItemForm';
 
-const BudgetView = (props) => (
-  <div>
-    <table>
-      <tbody>
-        <tr>
-          <td>Amount</td>
-          <td>{props.income}</td>
-        </tr>
-      </tbody>
-
-    </table>
-
-    <table>
-      <tbody>
-        <tr>
-          <td>Budget Item</td>
-          <td>Amount</td>
-        </tr>
-        {props.budgetItems.map((budgetItem, index) => (
-          <tr key={index}>
-            <td>{budgetItem.title}</td>
-            <td>{budgetItem.amount}</td>
+const BudgetView = (props) => {
+  return (
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <td>Amount</td>
+            <td>{props.income}</td>
           </tr>
-        ))}
-      </tbody>
+        </tbody>
 
-    </table>
+      </table>
 
-    <BudgetItemForm />
-  </div>
-);
+      <table>
+        <tbody>
+          <tr>
+            <td>Budget Item</td>
+            <td>Amount</td>
+          </tr>
+          {props.budgetItems.map((budgetItem, index) => (
+            <tr key={index}>
+              <td>{budgetItem.title}</td>
+              <td>{budgetItem.amount}</td>
+            </tr>
+          ))}
+        </tbody>
+
+      </table>
+
+      <BudgetItemForm {...props} />
+    </div>
+  )
+};
 
 BudgetView.propTypes = {
 
