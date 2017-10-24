101fa28ac1ad707a75786d7d87b8ef1fa87c5cf8
esurance <copo19131913@gmail.com>
2017-10-24 14:57:30 +0300

    add methods for getting index and item to App

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import IncomeForm from './components/IncomeForm';
 import BudgetView from './components/BudgetView';
-
+import BudgetItemForm from './components/BudgetItemForm';
 
 class App extends Component {
 
@@ -10,6 +10,7 @@ class App extends Component {
     super(props);
     this.state = {
       income: 0,
+      itemToEdit: {},
       budgetItems: [
         {
           title: "Transport",
@@ -17,22 +18,46 @@ class App extends Component {
         }
       ],
       incomeFormVisible: true,
+      currentBudgetItemIndex: null
     };
   }
   changeIncome = (newIncome) => {
     this.setState({ income: newIncome });
   };
 
-  // addBudgetItem = (prevState) => {
-  //   this.setState({
-  //     budgetItems: prevState.budgetItems.concat(prevState.item)
-  //   });
-  // };
-
   addBudgetItem = (item) => {
+    const budgetItems = [...this.state.budgetItems];
+    budgetItems.push(item);
     this.setState({
-      budgetItems: [...this.state.budgetItems, item]
+      budgetItems: budgetItems
     });
+  };
+
+  deleteBudgetItem = (index) => {
+    const budgetItems = [...this.state.budgetItems];
+    budgetItems.splice(index, 1);
+    this.setState({
+      budgetItems: budgetItems
+    });
+  };
+
+  setCurrentBudgetItemIndex = (index) => {
+    console.log('index', this.state.currentBudgetItemIndex);
+    this.setState({currentBudgetItemIndex:index});
+    
+  }
+  
+  editBudgetItem = (index) => {
+    const itemToEdit = this.state.budgetItems[index];
+    // const prevBudgetItems = [...this.state.budgetItems];
+    console.log('item to edit', itemToEdit)
+    this.setState({
+      itemToEdit: itemToEdit
+    })
+    // prevBudgetItems[index] = updatedItem;
+    // this.setState({
+    //   budgetItems: prevBudgetItems
+    // });
   };
 
   handleChange = (event) => {
@@ -43,7 +68,6 @@ class App extends Component {
     });
   }
 
-
   saveItems = (event) => {
     event.preventDefault()
     console.log(this.state);
@@ -51,11 +75,14 @@ class App extends Component {
   render() {
     const budgetItems = this.state.budgetItems;
     const income = this.state.income
-    const props = { budgetItems, income, addBudgetItem: this.addBudgetItem, handleChange: this.handleChange, saveItems: this.saveItems }
+    const itemToEdit = this.state.itemToEdit;
+
+    const props = { budgetItems, income, addBudgetItem: this.addBudgetItem, handleChange: this.handleChange, saveItems: this.saveItems, deleteBudgetItem: this.deleteBudgetItem, editBudgetItem: this.editBudgetItem, itemToEdit, setCurrentBudgetItemIndex: this.setCurrentBudgetItemIndex}
     return (
       <div>
         <IncomeForm income={this.state.income} changeIncome={this.changeIncome} />
         <BudgetView {...props} />
+        <BudgetItemForm addBudgetItem={this.addBudgetItem} itemToEdit={this.state.itemToEdit} />
       </div>
 
     );
